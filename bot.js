58b8815a4b2424a90299d6a9ebc4223ac1f03f71
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2019-04-21 23:54:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,13 +18,16 @@ client.on('ready', () => {
  message.delete(); 
 };     
 });
-var prefix="!"
- client.on("message", message => {
+var perfix ="!"
+client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "bco")) {
+            if (message.content.startsWith(prefix + "bco)) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
  message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
  message.delete(); 
 };     
