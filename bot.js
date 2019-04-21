556a4c2188114966f21ce4f08a5aa24e4b0d08d5
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2019-04-22 00:15:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- var prefix="!"
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -25,12 +24,10 @@ client.on("message", message => {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+  message.guild.members.filter(m => m.presence.status !== 'online').forEach(m => {
  m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
+ message.delete();      
+  })
 });
  var prefix = '!';
 
